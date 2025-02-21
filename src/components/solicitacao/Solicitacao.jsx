9dc80ece734b0aacc3342e6f0e6890c48bc5f910
Rejane444 <rejanekiss21@gmail.com>
2025-02-21 11:04:42 -0300

    últimas partes do desafio

--- src/components/solicitacao/Solicitacao.jsx
+++ src/components/solicitacao/Solicitacao.jsx
@@ -1,11 +1,75 @@
 import NavBar from "../navbar/NavBar.jsx"
+import styles from "./Solicitacao.module.scss"
+import Home from "../../assets/Dashboard/homeHeader.png"
+import Seta from "../../assets/Dashboard/Vector.png"
 
-function Solicitacao(){
-    return(
-        <>
-        <NavBar />
-        <h2>Sou o componente Solicitacao</h2>
-        </>
-    )
-    }
-    export default Solicitacao
+function Solicitacao() {
+
+    return (
+        <div className={styles.layoutSolicitacao}>
+            <NavBar />
+
+            <div className={styles.containerPrincipalSolicitacao}>
+                <header className={styles.headerSolicitacao}>
+                    <img src={Home} alt="Vetor da Casa" />
+                    <img src={Seta} alt="Vetor da Seta" />
+                    <p>Reembolsos</p>
+                    <img src={Seta} alt="Vetor da Seta" />
+                    <p>Solicitação de Reembolsos</p>
+
+                </header>
+
+ <main className={styles.mainSolicitacao}>
+          <form className={styles.formSolicitacao}>
+            <div className={styles.grupo1}>
+              <div className={styles.inputNome}>
+                <label htmlFor="">Nome Completo</label>
+                <input type="text" />
+              </div>
+
+              <div className={styles.inputEmpresa}>
+                <label htmlFor="">Empresa</label>
+                <input type="text" />
+              </div>
+
+              <div className={styles.inputPrestacao}>
+                <label htmlFor="">Nº Prest. Contas</label>
+                <input type="text" />
+              </div>
+
+              <div className={styles.inputMotivo}>
+                <label htmlFor=""> Descrição / Motivo do Reembolso </label>
+                <textarea name="" id="">
+                  {" "}
+                </textarea>
+              </div>
+            </div>
+
+            <div className={styles.barraVertical}></div>
+
+            <div className={styles.grupo2}>
+              <div className={styles.inputData}>
+                <label htmlFor="">Data</label>
+                <input type="date" name="" id="" />
+              </div>
+
+              <div className={styles.despesas}>
+                <label htmlFor="">  Tipo de Despesa </label>
+
+                <select name="" id="">
+                  <option value=""> Selecionar</option>
+                  <option value="">Alimentação</option>
+                  <option value="">Combustível</option>
+                </select>
+              </div>
+            </div>
+          </form>
+        </main>
+      </div>
+    </div>
+  );
+}
+                
+
+export default Solicitacao
+
